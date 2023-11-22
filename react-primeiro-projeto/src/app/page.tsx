25630fd79d52ec38d8084c5bd2c87681d03bd7f3
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2023-11-22 15:58:10 -0300

    Primeiro componente em react

---
+++ react-primeiro-projeto/src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Circle, Square } from "./components/Geo";
+
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Olá Mundo</h1>
+      <h3>Texto qualquer</h3>
+      <Square />
+      <Circle />
+    </div>
+  );
+};
+
+export default Page;
+
