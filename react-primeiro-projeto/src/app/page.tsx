f96ddd2fc1cec4b06f4817425d76d987df7027e8
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2023-11-22 23:50:02 -0300

    Usando componentes dentro de componentes

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Circle, Square } from "./components/Geo";
-
+import { GeoForm } from "./components/GeoForm";
 
 const Page = () => {
   return (
     <div>
       <h1>Olá Mundo</h1>
       <h3>Texto qualquer</h3>
-      <Square />
-      <Circle />
+      <GeoForm />
     </div>
   );
 };
