1f50d20721d615d8bf793e83731ecebfe0a77d93
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2023-11-24 13:07:04 -0300

    exercicio greeting condicional

--- react-primeiro-projeto/src/app/data/peopleList.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from "../types/Person";
-
-export const peopleList: Person[] = [
-    { id: 1, name: 'Fulano', profession: 'Developer' },
-    { id: 2, name: 'Ciclano', profession: 'Tester' },
-    { id: 3, name: 'Beltrano', profession: 'Manager' },
-    { id: 4, name: 'João', profession: 'Chemist' },
-    { id: 5, name: 'Maria', profession: 'Physicist' },
-];
--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,19 +1,28 @@
-import { peopleList } from '@/app/data/peopleList';
+const currentTime =  new Date().toLocaleTimeString("pt-br", {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+const greeting = () => {
+   if (currentTime >= "0" && currentTime < "12") {
+     return "Bom dia";
+   } else if (currentTime >= "12" && currentTime < "18") {
+     return "Boa tarde";
+   } else {
+     return "Boa noite";
+   }
+    };
 
 const Page = () => {
-
   return (
-    <div>
-      <h1>Olá Mundo</h1>
-      <h3>Texto qualquer</h3>
-
-
-      <ul>
-        {peopleList.map(person => <li key={person.id}>{person.name} - {person.profession} </li>)}
-      </ul>
+    <div className="bg-gradient-to-br from-blue-500 to-blue-950 text-white w-screen h-screen flex flex-col items-center justify-center">
+      <div className='text-9xl'>{currentTime}</div>
+      <div className='text-5xl font-bold'>{greeting()}</div>
     </div>
   );
 };
+
+
 
 export default Page;
 
