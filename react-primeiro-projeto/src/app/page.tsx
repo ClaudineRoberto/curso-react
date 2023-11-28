ede3feb29d17ad344a077b15c14bb1ee40f3d5c0
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2023-11-28 12:39:19 -0300

    Aprendendo States

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,18 +1,15 @@
-
-import React from "react";
-import { StudentTable } from "./components/StudentTable";
-import { students } from "./data/students";
+"use client"
 
 const Page = () => {
-  return (
-    <div className="container mx-auto">
-      <h1 className="text-5xl mb-5">Lista de estudantes</h1>
-      <StudentTable students={students} />  
+
+
+
+  return(
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+
     </div>
-    
   );
 };
 
-
-
 export default Page;
+
