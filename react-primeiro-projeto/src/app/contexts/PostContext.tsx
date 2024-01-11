b4c9123a56d2680b022ff6cb7642c2ad77da09e4
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-01-11 12:11:46 -0300

    salvando informações no localstorage

--- react-primeiro-projeto/src/app/contexts/PostContext.tsx
+++ react-primeiro-projeto/src/app/contexts/PostContext.tsx
@@ -1,6 +1,8 @@
-import { ReactNode, createContext, useReducer, useState } from "react";
+import { ReactNode, createContext, useEffect, useReducer, useState } from "react";
 import { Post } from "../types/Post";
 import { postReducer } from "../reducers/postReducers";
+
+const STORAGE_KEY = 'postContextContent';
 
 type PostContextType = {
     posts: Post[];
@@ -13,7 +15,7 @@ export const PostContext = createContext<PostContextType | null>(null);
 
 export const PostProvider = ({children}: {children: ReactNode}) => {
 
-    const [posts, dispatch] = useReducer(postReducer, []);
+    const [posts, dispatch] = useReducer(postReducer, JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]') );
 
     const addPost = (title: string, body: string ) => {
         if(title.trim() !== '' && body.trim() !== ''){
@@ -31,9 +33,14 @@ export const PostProvider = ({children}: {children: ReactNode}) => {
         })
     }
 
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(posts));
+    }, [posts]);
+
     return(
         <PostContext.Provider value={{posts, addPost, removePost}}>
             {children}
         </PostContext.Provider>
     );
+
 }
