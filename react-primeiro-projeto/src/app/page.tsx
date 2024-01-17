edcf44a4455b06210a2615fd906af120ebbf1792
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-01-17 12:48:46 -0300

    Envio de imagens usando fetch method POST

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,18 +1,68 @@
 "use client"
 
-import { Chat } from "./components/Chat";
-import { ChatProvider } from "./contexts/ChatContext";
-import { UserProvider } from "./contexts/UserContext";
+import { useRef, useState } from "react";
+
 
 const page = () => {
+
+  const [legendInput, setLegendInput] = useState('')
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handleFileSend = async () => {
+    //verificando se existe o campo files dentro do campo input
+    if(fileInputRef.current?.files && fileInputRef.current.files.length > 0){
+      const fileItem = fileInputRef.current.files[0]
+      //verificando se o arquivo é uma imagem
+      const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg']
+
+      if (allowedTypes.includes(fileItem.type)){
+
+        const data = new FormData();
+        data.append('image', fileItem);
+        data.append('legend', legendInput);
+
+        const res = await fetch ('https://jsonplaceholder.typicode.com/posts', {
+          method: 'POST',
+          body: data,
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+
+        const json = await res.json();
+        console.log(json);
+
+      } else {
+        alert('Tipo de arquivo não permitido');
+      }
+
+    } else {
+      alert('Você precisa selecionar um arquivo');
+    
+    }
+  }
+  
     return (
-        <div className="container mx-auto max-w-lg px-2">
-          <UserProvider>
-            <ChatProvider>
-              <h1 className=" text-3xl my-3 text-center">Chat</h1>
-              <Chat />
-            </ChatProvider>
-          </UserProvider>
+        <div className="container mx-auto">
+          <h1 className="text-3xl mt-4">Upload de Imagem</h1>
+            
+            <div className="flex flex-col gap-3 max-w-md border border-white p-3 mt-4">
+              <input 
+              type="file"
+              ref={fileInputRef}
+              />
+              <input 
+              type="text" 
+              placeholder="Digite uma legenda"
+              className="p-3 bg-white rounded-md text-black"
+              value={legendInput}
+              onChange={(event) => setLegendInput(event.target.value)}
+              />
+              <button 
+              className="border-blue-900 bg-blue-700 w-28 px-3 py-2 rounded-md"
+              onClick={handleFileSend}
+              >Enviar</button>
+            </div>
         </div>
     );
 }
