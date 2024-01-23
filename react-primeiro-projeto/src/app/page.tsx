ee7fbb356a64fe6243f92e79d9af1bee1c0b955c
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-01-23 10:00:42 -0300

    Instalando o tanstack

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,29 +1,11 @@
 "use client"
-import axios from "axios";
 
 
 const page = () => {
 
-  const controler = new AbortController();
-
-  const handleStartRequest = async () => {
-    try{
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          signal: controler.signal
-        });
-    } catch(error){
-      console.log('Deu algum problema...')
-    }
-  }
-
-  const handleCancelRequest = () => {
-    controler.abort();
-  }
-
-  return (
-        <div className="container mx-auto flex gap-3 mt-4">
-            <button onClick={handleStartRequest} className="border border-white rounded-md px-3 py-4">Fazer requisição</button>
-            <button onClick={handleCancelRequest} className="border border-white rounded-md px-3 py-4">Cancelar requisição</button>
+    return (
+        <div className="container mx-auto">
+          <h1 className="text-white text-3xl">Salveeee</h1>
         </div>
     );
 }
