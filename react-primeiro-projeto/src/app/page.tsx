e52801798d32c7bd275bf98245637a17f0d5c477
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-01-23 11:20:43 -0300

    Fazendo uma query com tanstack

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,11 +1,35 @@
 "use client"
 
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Posts } from "./types/Posts";
 
 const page = () => {
 
+  const query = useQuery({
+    queryKey: ['posts'],
+    queryFn: async (): Promise<Posts[]> => {
+      const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      return result.data;
+    }
+  });
+
+
+
     return (
         <div className="container mx-auto">
-          <h1 className="text-white text-3xl">Salveeee</h1>
+          <h1 className="text-white text-3xl">Lista de posts</h1>
+          {query.isLoading && <p className="text-white">Carregando...</p>}
+
+          {query.data &&
+            <ul>
+              {query.data.map(item => (
+                <li key={item.id} className="text-white">{item.title}</li>
+              ))}
+            </ul>
+          }
+          
+
         </div>
     );
 }
