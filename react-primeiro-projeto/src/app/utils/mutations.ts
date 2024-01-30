83ab8d3b095f69dc65feef8b7f6c0f6bb2e98576
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-01-30 15:59:47 -0300

    Atualizando Query pelo Mutation (não recomendado)

--- react-primeiro-projeto/src/app/utils/mutations.ts
+++ react-primeiro-projeto/src/app/utils/mutations.ts
@@ -1,14 +1,14 @@
 import { useMutation } from "@tanstack/react-query"
 import { addPost } from "./api"
 import { queryClient } from "./querycClient";
+import { Post } from "../types/Post";
 
 export const useAddPost = () => {
     const mutation = useMutation({
         mutationFn: addPost,
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ['posts']
-            });
+        onSuccess: (newPost: Post) => {
+            const posts = queryClient.getQueryData(['posts']) as Post[];
+            queryClient.setQueryData(['posts'], [newPost, ...posts]);
         }
     });
     return mutation;
